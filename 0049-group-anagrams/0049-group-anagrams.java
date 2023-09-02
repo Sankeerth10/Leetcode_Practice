21cd0d262d72855e289193282683bdf25ad6c0fd
Sankeerth Koduri <100099621+Sankeerth10@users.noreply.github.com>
2023-09-02 19:54:36 -0400

    Time: 5 ms (99.21%), Space: 46.9 MB (56.52%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> res = new HashMap<>();
+        for(String s: strs){
+            char []charArr = s.toCharArray();
+            Arrays.sort(charArr); 
+            String a = String.valueOf(charArr);
+            if(!res.containsKey(a)){
+                ArrayList<String> arr = new ArrayList<String>();
+                res.put(a, arr);
+            }
+            res.get(a).add(s);
+        }
+        return new ArrayList(res.values());
+    }
+}
