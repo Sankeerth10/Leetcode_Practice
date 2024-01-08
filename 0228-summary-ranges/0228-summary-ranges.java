4a95399129d3032be287e24024fa7b1c440f91c6
Sankeerth Koduri <100099621+Sankeerth10@users.noreply.github.com>
2024-01-08 14:01:10 -0500

    Time: 5 ms (85.60%), Space: 42.2 MB (5.43%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        ArrayList<String> res = new ArrayList<>();
+        int index = 0;
+        while (index < nums.length) {
+            int temp = index;
+            while (((temp + 1) < nums.length) && (nums[temp + 1]  == nums[temp] + 1)) {
+                temp++;
+            }
+            if (index == temp) {
+                res.add(nums[index] + "");
+            }
+            else {
+                res.add(nums[index] + "->" + nums[temp]);
+                index = temp;
+            }
+            index++;
+        }
+        
+        return res;
+    }
+}
