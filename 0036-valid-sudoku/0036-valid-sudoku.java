eaf65d00d727b1f886fbdd7e3ee899f8d573fea1
Sankeerth Koduri <100099621+Sankeerth10@users.noreply.github.com>
2024-02-10 11:10:48 -0500

    Time: 15 ms (18.18%), Space: 45.7 MB (6.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,21 @@
 class Solution {
-   public boolean isValidSudoku(char[][] board) {
-    Set seen = new HashSet();
-    for (int i=0; i<9; i++) {
-        for (int j=0; j<9; j++) {
-            if (board[i][j] != '.') {
-                String b = "(" + board[i][j] + ")";
-                if (!seen.add(b + "in row" +i) || !seen.add(b + "in colmn" + j) || !seen.add(i/3 + b + j/3))
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> unq = new HashSet<>();
+        for(int i = 0; i < 9; i ++) {
+            for(int j =0; j < 9; j++) {
+                char current = board[i][j];
+                if(current != '.') {
+                    if(unq.contains(current + " in row " + i) || unq.contains(current + " in column " + j) || unq.contains(current + " in row " + i/3 + " in column " + j/3)) {
                     return false;
+                    }
+                    else {
+                        unq.add(current + " in row " + i);
+                        unq.add(current + " in column " + j);
+                        unq.add(current + " in row " + i/3 + " in column " + j/3);
+                    }
+                } 
             }
         }
+        return true;
     }
-    return true;
-}
 }
