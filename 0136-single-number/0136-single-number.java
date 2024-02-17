cca935625c8640e78583f8b4d5570fb12e9eb4e0
Sankeerth Koduri <100099621+Sankeerth10@users.noreply.github.com>
2024-02-17 13:26:12 -0500

    Time: 1 ms (99.86%), Space: 46.1 MB (49.86%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,32 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+        // if (nums.length == 1) {
+        //     return nums[0];
+        // }
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i =0; i < nums.length; i++) {
+        //     if(!map.containsKey(nums[i])){
+        //         map.put(nums[i], 1);
+        //     }
+        //     else {
+        //         int val = map.get(nums[i]);
+        //         map.put(nums[i], val+1);
+        //     }
+        // }
+        // Integer foundKey = null;
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if (entry.getValue().equals(1)) {
+        //         foundKey = entry.getKey();
+        //         break;
+        //     }
+        // }
+        // return (int) foundKey;
+
+    //SolOptimal:
+        int a = 0;
+        for (int num : nums) {
+            a = a^num; 
         }
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i =0; i < nums.length; i++) {
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i], 1);
-            }
-            else {
-                int val = map.get(nums[i]);
-                map.put(nums[i], val+1);
-            }
-        }
-        Integer foundKey = null;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue().equals(1)) {
-                foundKey = entry.getKey();
-                break;
-            }
-        }
-        return (int) foundKey;
+        return a;
     }
 }
