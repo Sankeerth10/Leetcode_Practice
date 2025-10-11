3feecc52ab9fad8ff6de4d0deca19fa6f88bf726
Sankeerth Koduri <100099621+Sankeerth10@users.noreply.github.com>
2025-10-11 16:27:39 -0400

    Time: 0 ms (100.00%) | Memory: 44.7 MB (35.55%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,22 +14,43 @@
  * }
  */
 class Solution {
+    // public int kthSmallest(TreeNode root, int k) {
+    //     List<Integer> list = new ArrayList<>();
+    //     dfs(root, list);
+    //     Collections.sort(list);
+    //     return list.get(k-1);
+    // }
+
+    // void dfs(TreeNode node, List<Integer> list) {
+    //     list.add(node.val);
+    //     if (node.left != null) {
+    //         dfs(node.left, list);
+    //     }
+    //     if (node.right != null) {
+    //         dfs(node.right, list);
+    //     }
+
+    //     return;
+    // }
+
+    int count = 0;
+    TreeNode result = null;
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> list = new ArrayList<>();
-        dfs(root, list);
-        Collections.sort(list);
-        return list.get(k-1);
+        inorderTraversal(root, k);
+        return result.val;
     }
 
-    void dfs(TreeNode node, List<Integer> list) {
-        list.add(node.val);
-        if (node.left != null) {
-            dfs(node.left, list);
-        }
-        if (node.right != null) {
-            dfs(node.right, list);
+    private void inorderTraversal(TreeNode node, int k) {
+        if(node == null) {
+            return;
         }
 
-        return;
+        inorderTraversal(node.left, k);
+        count++;
+        if(count == k) {
+            result = node;
+            return;
+        }
+        inorderTraversal(node.right, k);
     }
 }
